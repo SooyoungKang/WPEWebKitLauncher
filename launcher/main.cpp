d25c912a1269e1b39d86dd15195681431ea3a426
Soo Young Kang <sooyoung.kang@hp.com>
2018-02-13 09:13:33 +0900

    Copy origin WPE Launcher src

---
+++ launcher/main.cpp
@@ -0,0 +1,261 @@
+/*
+ * Copyright (C) 2015, 2016 Igalia S.L.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <WPE/WebKit.h>
+#include <WPE/WebKit/WKCookieManagerSoup.h>
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <glib.h>
+#include <initializer_list>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+WKPageNavigationClientV0 s_navigationClient = {
+    { 0, nullptr },
+    // decidePolicyForNavigationAction
+    [](WKPageRef, WKNavigationActionRef, WKFramePolicyListenerRef listener, WKTypeRef, const void*) {
+        WKFramePolicyListenerUse(listener);
+    },
+    // decidePolicyForNavigationResponse
+    [](WKPageRef, WKNavigationResponseRef, WKFramePolicyListenerRef listener, WKTypeRef, const void*) {
+        WKFramePolicyListenerUse(listener);
+    },
+    nullptr, // decidePolicyForPluginLoad
+    nullptr, // didStartProvisionalNavigation
+    nullptr, // didReceiveServerRedirectForProvisionalNavigation
+    nullptr, // didFailProvisionalNavigation
+    nullptr, // didCommitNavigation
+    nullptr, // didFinishNavigation
+    nullptr, // didFailNavigation
+    nullptr, // didFailProvisionalLoadInSubframe
+    // didFinishDocumentLoad
+    [](WKPageRef page, WKNavigationRef, WKTypeRef, const void*) {
+        WKStringRef messageName = WKStringCreateWithUTF8CString("Hello");
+        WKMutableArrayRef messageBody = WKMutableArrayCreate();
+
+        for (auto& item : { "Test1", "Test2", "Test3" }) {
+            WKStringRef itemString = WKStringCreateWithUTF8CString(item);
+            WKArrayAppendItem(messageBody, itemString);
+            WKRelease(itemString);
+        }
+
+        fprintf(stderr, "[WPELauncher] Hello InjectedBundle ...\n");
+        WKPagePostMessageToInjectedBundle(page, messageName, messageBody);
+        WKRelease(messageBody);
+        WKRelease(messageName);
+    },
+    nullptr, // didSameDocumentNavigation
+    nullptr, // renderingProgressDidChange
+    nullptr, // canAuthenticateAgainstProtectionSpace
+    nullptr, // didReceiveAuthenticationChallenge
+    // webProcessDidCrash
+    [](WKPageRef page, const void*) {
+        fprintf(stderr, "WARNING: WebProcess crashed: restarting it ...\n");
+        WKPageReload(page);
+    },
+    nullptr, // copyWebCryptoMasterKey
+    nullptr, // didBeginNavigationGesture
+    nullptr, // willEndNavigationGesture
+    nullptr, // didEndNavigationGesture
+    nullptr, // didRemoveNavigationGestureSnapshot
+};
+
+int main(int argc, char* argv[])
+{
+    const char* url = "http://youtube.com/tv";
+#ifdef WIN32
+    const char * injected_bundle_path = nullptr;
+#else
+    const char * injected_bundle_path = "/usr/lib/libWPEInjectedBundle.so";
+#endif
+    int width = 0, height = 0, style = -1;
+
+    for (int i = 1; i < argc; i++) {
+      const char * arg = argv[i];
+      if (strcmp(arg, "--injected-bundle-path") == 0) {
+        if (i == argc - 1) {
+          fprintf(stderr, "--injected-bundle-path requires another argument - the path to the injected bundle\n");
+          return -1;
+        }
+        else {
+          injected_bundle_path = argv[i + 1];
+          i++;
+        }
+      }
+      else if (strcmp(arg, "--width") == 0) {
+          if (i == argc - 1) {
+              fprintf(stderr, "--width requires another argument - the display width\n");
+              return -1;
+          }
+          else {
+              width = atoi(argv[i + 1]);
+              i++;
+          }
+      }
+      else if (strcmp(arg, "--height") == 0) {
+          if (i == argc - 1) {
+              fprintf(stderr, "--height requires another argument - the display height\n");
+              return -1;
+          }
+          else {
+              height = atoi(argv[i + 1]);
+              i++;
+          }
+      }
+      else {
+        url = arg;
+      }
+    }
+
+#ifdef WIN32
+    if (!injected_bundle_path) {
+        fprintf(stderr, "Error: no --injected-bundle-path arg passed\n");
+        return -1;
+    }
+#endif
+
+    {
+      struct stat buffer;
+      if (stat(injected_bundle_path, &buffer) != 0) {
+        fprintf(stderr, "Error: injected bundle %s not found\n", injected_bundle_path);
+        return -1;
+      }
+    }
+
+    GMainLoop* loop = g_main_loop_new(nullptr, FALSE);
+
+    auto contextConfiguration = WKContextConfigurationCreate();
+    auto injectedBundlePath = WKStringCreateWithUTF8CString(injected_bundle_path);
+    WKContextConfigurationSetInjectedBundlePath(contextConfiguration, injectedBundlePath);
+
+    gchar *wpeStoragePath = g_build_filename(g_get_user_cache_dir(), "wpe", "local-storage", nullptr);
+    g_mkdir_with_parents(wpeStoragePath, 0700);
+    auto storageDirectory = WKStringCreateWithUTF8CString(wpeStoragePath);
+    g_free(wpeStoragePath);
+    WKContextConfigurationSetLocalStorageDirectory(contextConfiguration, storageDirectory);
+
+    gchar *wpeDiskCachePath = g_build_filename(g_get_user_cache_dir(), "wpe", "disk-cache", nullptr);
+    g_mkdir_with_parents(wpeDiskCachePath, 0700);
+    auto diskCacheDirectory = WKStringCreateWithUTF8CString(wpeDiskCachePath);
+    g_free(wpeDiskCachePath);
+    WKContextConfigurationSetDiskCacheDirectory(contextConfiguration, diskCacheDirectory);
+
+    WKRelease(injectedBundlePath);
+
+    WKContextRef context = WKContextCreateWithConfiguration(contextConfiguration);
+    WKRelease(contextConfiguration);
+
+    auto pageGroupIdentifier = WKStringCreateWithUTF8CString("WPEPageGroup");
+    auto pageGroup = WKPageGroupCreateWithIdentifier(pageGroupIdentifier);
+    WKRelease(pageGroupIdentifier);
+
+    auto preferences = WKPreferencesCreate();
+
+#if USING_WPE_WEBKIT
+    // These methods were introduced in the WPE webkit fork but never went upstream
+    // Allow mixed content.
+    WKPreferencesSetAllowRunningOfInsecureContent(preferences, true);
+    WKPreferencesSetAllowDisplayOfInsecureContent(preferences, true);
+    WKPreferencesSetWebSecurityEnabled(preferences, false);
+
+    // By default allow console log messages to system console reporting.
+    if (!g_getenv("WPE_SHELL_DISABLE_CONSOLE_LOG"))
+      WKPreferencesSetLogsPageMessagesToSystemConsoleEnabled(preferences, true);
+#endif
+
+    WKPageGroupSetPreferences(pageGroup, preferences);
+
+    auto pageConfiguration  = WKPageConfigurationCreate();
+    WKPageConfigurationSetContext(pageConfiguration, context);
+    WKPageConfigurationSetPageGroup(pageConfiguration, pageGroup);
+    WKPreferencesSetFullScreenEnabled(preferences, true);
+
+    if (!!g_getenv("WPE_SHELL_COOKIE_STORAGE")) {
+      gchar *cookieDatabasePath = g_build_filename(g_get_user_cache_dir(), "cookies.db", nullptr);
+      auto path = WKStringCreateWithUTF8CString(cookieDatabasePath);
+      g_free(cookieDatabasePath);
+      auto cookieManager = WKContextGetCookieManager(context);
+      WKCookieManagerSetCookiePersistentStorage(cookieManager, path, kWKCookieStorageTypeSQLite);
+    }
+
+    auto view = WKViewCreate(pageConfiguration);
+
+    if (width != 0 && height != 0)
+        WKViewSetSizeAndStyle(view, width, height, style);
+
+    struct capture_stuff
+    {
+        GMainLoop* mainLoop;
+    };
+    capture_stuff capture{ loop };
+
+    WKViewClientV1 s_viewClient = {
+        { 1, &capture },
+        // frameDisplayed
+        [](WKViewRef, const void*) {
+            static unsigned s_frameCount = 0;
+            static gint64 lastDumpTime = g_get_monotonic_time();
+
+            if (!g_getenv("WPE_DISPLAY_FPS"))
+                return;
+
+            ++s_frameCount;
+            gint64 time = g_get_monotonic_time();
+            if (time - lastDumpTime >= 5 * G_USEC_PER_SEC) {
+                fprintf(stderr, "[WPELauncher] %.2f FPS\n",
+                    s_frameCount * G_USEC_PER_SEC * 1.0 / (time - lastDumpTime));
+                s_frameCount = 0;
+                lastDumpTime = time;
+            }
+        },
+        //quitRequested
+        [](WKViewRef view, const void* clientInfo) {
+            const capture_stuff * capture = reinterpret_cast<const capture_stuff*>(clientInfo);
+            g_main_loop_quit(capture->mainLoop);
+        }
+    };
+
+    WKViewSetViewClient(view, &s_viewClient.base);
+
+    auto page = WKViewGetPage(view);
+    WKPageSetPageNavigationClient(page, &s_navigationClient.base);
+
+    auto shellURL = WKURLCreateWithUTF8CString(url);
+    WKPageLoadURL(page, shellURL);
+    WKRelease(shellURL);
+
+    g_main_loop_run(loop);
+
+    WKRelease(view);
+    WKRelease(pageConfiguration);
+    WKRelease(pageGroup);
+    WKRelease(context);
+    WKRelease(preferences);
+    g_main_loop_unref(loop);
+    return 0;
+}
